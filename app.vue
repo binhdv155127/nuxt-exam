3d699026a386fef5389e8cc7b7b16d88d18bee00
binhdv <duongvanbinh3111997@gmail.com>
2023-11-20 09:54:57 +0700

    commit

--- app.vue
+++ app.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
    <h1>Welcome to the homepage</h1>
-   <h2>xin chào</h2>
-   <div>thêm ảnh</div>
+   <!-- <h2>xin chào</h2>
+   <div>thêm ảnh</div> -->
    <!-- <img src="~/assets/0516.png" alt="" style="width: 200px;"> -->
-   <div>thêm dòng</div>
+   <!-- <div>thêm dòng</div>
    <div>tesst</div>
    <div>tesst code build</div>
    <div>code build 1</div>
    <div>code build 2</div>
    <div>code build 3</div>
    <div>code build 5</div>
-   <div>test 111</div>
+   <div>test 111</div> -->
+   <div>auto deploy by codePipeline</div>
   </div>
 </template>
